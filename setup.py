a8a63f2699ed9fdb537363c3b7bbd7b8624f02e3
DiNAMitiON <111492457+DiNAMitiON@users.noreply.github.com>
2023-10-31 12:13:14 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md") as file:
     read_me_description = file.read()
 
 setuptools.setup(
-    name="spetsloging",
-    version="dev-0.1",
+    name="spetslogging",
+    version="alfa-1.0",
     author="Dmitry",
     author_email="dinamition42@gmail.com",
-    description="This is a test package.",
+    description="SpetsLogging is a simple way to log in the console and in a file.",
     long_description=read_me_description,
     long_description_content_type="text/markdown",
-    url="package_github_page",
+    url="https://github.com/DiNAMitiON/SpetsLogging",
     packages=['spetsloging'],
     classifiers=[
         "Programming Language :: Python :: 3",
